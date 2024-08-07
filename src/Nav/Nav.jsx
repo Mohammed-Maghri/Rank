a1584428cc2271f39ce83bcd8fd0af0cab39badb
Mohammd Maghri <ouedzoum69@gmail.com>
2024-08-08 00:16:50 +0100

    Picture Problem Fixed

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -48,7 +48,7 @@ export function Nav() {
         </div>
         <div className=" flex items-center justify-center flex-row rounded-[5px] h-[95%] w-[25%]">
           <div style={{boxShadow: '0px 0px 5px yellow'}}  className="cursor-pointer w-[50px] h-[50px] border-solid border-white border-[2px] rounded-[50%]">
-            <img src={all[0].profileImage} onClick={() => (functionVisible(visible, setVisible))} className="rounded-[50%]" />
+            <img src={all[0].profileImage} onClick={() => (functionVisible(visible, setVisible))} className="rounded-[50%] h-[100%] w-[100%] object-cover" />
           </div>
             {visible &&
                 <div onClick={functionLogout} className="absolute cursor-pointer flex top-[60px] font-bars2 items-center justify-center  w-[100px] h-[30px] rounded-[20px] bg-white duration-300 hover:bg-slate-300">
