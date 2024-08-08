5a8851f68606c60d7c7d5d05f5f37761ed4e9fc4
Mohammd Maghri <ouedzoum69@gmail.com>
2024-08-08 01:11:10 +0100

    Problem with the Cookie

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -81,7 +81,7 @@ useEffect(() => {
 	const val  =  new URLSearchParams(window.location.search).get('code');
 	test.code = val;
 	console.log(test.code);
-	if (Cookies.get('access_token') || Cookies.get('access_token') == undefined) {
+	if (!Cookies.get('access_token') || Cookies.get('access_token') == undefined) {
 		functionapi('https://leets1337-3f387c570577.herokuapp.com/api/v1/authenticate');
 	}
 }, [])
