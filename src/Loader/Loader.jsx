8372e61707b33a36dcb25cfd5a8d6a7837599529
Mohammed Maghri <mmaghri@e3r8p10.1337.ma>
2024-08-08 21:25:17 +0100

    Loading animation added

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import "./Loader.css";
 export default function Loads() {
     return (
         <div className="w-[400px] h-[400px] flex items-center justify-center">
-            <span className="loading loading-ball w-[100%] h-[100%] bg-yellow-500  loading-md"></span>
+            <span className="loading loading-ring w-[100%] h-[100%] bg-yellow-500  loading-md"></span>
         </div>
     );
 }
