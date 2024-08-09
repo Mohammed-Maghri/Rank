d35d741e6a9407e4648eb5b4e810555d2e3a1baa
Mohammed Maghri <mmaghri@e3r10p4.1337.ma>
2024-08-09 12:19:25 +0100

    Adding the New bar

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -33,34 +33,34 @@ export function Nav() {
       navigate("/");
     }
     return ( 
-        <div className="flex items-center justify-ithems-center h-[80px] gap-[5px] max-w-[1800px]  w-[100%]">
-        <div className="flex items-center  justify-center rounded-[5px] h-[95%] w-[25%]">
-          <div className="flex items-center justify-center w-[50px] h-[50px] ml-[10px]  bg-yellow-300 rounded-[10px]">
-            <IoSettings className=" text-black  w-[34px] h-[35px] duration-200 cursor-pointer hover:scale-105" />
-          </div>
-        </div>
-        <div className="w-[50%] flex items-center justify-center h-[95%]  rounded-[5px] h-[95%]">
-          <div style={{boxShadow: '0px 0px 10px yellow'}} className="flex items-center xs:gap-1 gap-5 justify-center duration-300 lg:w-[600px] 
-          sm:w-[400px] xs:w-[200px] custom-yellow h-[60%] bg-yellow-300 rounded-[30px]">
-            {object.map((item, index) => (
-              <div className="font-bars text-lg text-black font-medium xs:text-sm flex items-center cursor-pointer justify-center w-[150px] h-[90%] duration-300 hover:scale-110">
-                <p key={index}> {item.bars} </p>
-              </div>
-              ))
-            }
-
-          </div>
-        </div>
-        <div className=" flex items-center justify-center flex-row rounded-[5px] h-[95%] w-[25%]">
-          <div style={{boxShadow: '0px 0px 5px yellow'}}  className="cursor-pointer w-[50px] h-[50px] border-solid border-white border-[2px] rounded-[50%]">
-            <img src={taj} onClick={() => (functionVisible(visible, setVisible))} className="rounded-[50%] h-[100%] w-[100%] object-cover" />
-          </div>
-            {visible &&
-                <div onClick={functionLogout} className="absolute cursor-pointer flex top-[60px] font-bars2 items-center justify-center  w-[100px] h-[30px] rounded-[20px] bg-white duration-300 hover:bg-slate-300">
-                  <p  className="text-black font-extrabold"> Logout </p>
-                </div>
-            }
+      <div className="flex items-center justify-ithems-center h-[80px] gap-[5px] max-w-[1800px]  w-[100%]">
+      <div className="flex items-center  justify-center rounded-[5px] h-[95%] w-[25%]">
+        <div className="flex items-center justify-center w-[50px] h-[50px] ml-[10px]  bg-yellow-200 border-solid border-yellow-500 border-[3px] rounded-[10px]">
+          <IoSettings className=" text-black  w-[34px] h-[35px] duration-200 cursor-pointer hover:scale-105" />
         </div>
       </div>
+      <div className="w-[50%] flex items-center justify-center h-[95%]  rounded-[5px] h-[95%]">
+        <div style={{boxShadow: '0px 0px 10px yellow'}} className="flex items-center xs:gap-1 gap-5 justify-center duration-300 lg:w-[800px] 
+        sm:w-[400px] xs:w-[200px] custom-yellow h-[60%] bg-yellow-200 border-solid border-yellow-500 border-[3px] rounded-[15px]">
+          {object.map((item, index) => (
+            <div className="font-bars2  text-lg text-black font-bold xs:text-sm flex items-center cursor-pointer justify-center w-[150px] h-[90%] duration-300 hover:scale-110">
+              <p key={index}> {item.bars} </p>
+            </div>
+            ))
+          }
+
+        </div>
+      </div>
+      <div className=" flex items-center justify-center flex-row rounded-[5px] h-[95%] w-[25%]">
+        <div style={{boxShadow: '0px 0px 5px yellow'}}  className="cursor-pointer w-[50px] h-[50px] border-solid border-white border-[2px] rounded-[50%]">
+          <img src={taj} onClick={() => (functionVisible(visible, setVisible))} className="rounded-[50%] h-[100%] w-[100%] object-cover" />
+        </div>
+          {visible &&
+              <div onClick={functionLogout} className="absolute cursor-pointer flex top-[60px] font-bars2 items-center justify-center  w-[100px] h-[30px] rounded-[20px] bg-white duration-300 hover:bg-slate-300">
+                <p  className="text-black font-extrabold"> Logout </p>
+              </div>
+          }
+      </div>
+    </div>
     );
 }
