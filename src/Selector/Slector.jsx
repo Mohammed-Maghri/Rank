5afc6023d3b7e17d41b5bbec514f9ba9bb45110c
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-10 09:59:16 +0100

    Testing  The Switch

--- src/Selector/Slector.jsx
+++ src/Selector/Slector.jsx
@@ -26,7 +26,6 @@ export function Selector() {
     const object = [{promo: 2018}, {promo: 2019}, {promo: 2020}, {promo: 2021}, {promo: 2022}, {promo: 2023}, {promo: 2024}];
   
     const functionGet =  (url) => {
-      setPages(() => (1));
       Cookies.set('poolYear', addyear);
       Cookies.set('cursusId', pol);
       Cookies.set('month', month);
@@ -131,7 +130,7 @@ export function Selector() {
           }
         </div>
       </div>
-      <div onClick={() => (setLoadingstate(true), functionGet("https://leets1337-3f387c570577.herokuapp.com/api/v1/home"))} className="ml-4 w-[30px] h-[30px] flex items-center justify-center  border-solid border-white duration-200 bg-slate-900 rounded-[10px] border-[2px] cursor-pointer border-opacity-20 hover:scale-105">
+      <div onClick={() => (setLoadingstate(true), setPages((0)), functionGet("https://leets1337-3f387c570577.herokuapp.com/api/v1/home"))} className="ml-4 w-[30px] h-[30px] flex items-center justify-center  border-solid border-white duration-200 bg-slate-900 rounded-[10px] border-[2px] cursor-pointer border-opacity-20 hover:scale-105">
           <FaSearch   className="w-[15px] h-[15px] text-white" />
           </div>
     </div>
