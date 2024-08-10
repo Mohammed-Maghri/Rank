a6ebcc63aa38993e11081d118b5208b9a88a18bc
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-10 10:06:06 +0100

    Fixing the Load Error

--- src/Selector/Slector.jsx
+++ src/Selector/Slector.jsx
@@ -100,13 +100,13 @@ export function Selector() {
                     </div>
                   </div>
                   <div className="w-[100%] flex-row flex items-center justify-center h-[30px]">
-                    <div onClick={() => (month == 0 ? (setMoth(12)) : (setMoth(month - 1)))} className="w-[30%] cursor-pointer h-[100%] flex items-center justify-center">
+                    <div onClick={() => (month <= 12 ? (setMoth(0)) : (setMoth(month + 1)))} className="w-[30%] cursor-pointer h-[100%] flex items-center justify-center">
                         <FaChevronLeft className="text-black"/>
                     </div>
                     <div className="w-[40%] text-black flex items-center justify-center h-[100%]">
                       <p>{month}</p>
                     </div>
-                    <div onClick={() => (month <= 12 ? (setMoth(0)) : (setMoth(month + 1)))} className="w-[30%] cursor-pointer h-[100%] flex items-center justify-center">
+                    <div onClick={() => (month == 1 ? (setMoth(12)) : (setMoth(month - 1)))} className="w-[30%] cursor-pointer h-[100%] flex items-center justify-center">
                         <FaChevronRight className="text-black"/>
                     </div>
                   </div>
