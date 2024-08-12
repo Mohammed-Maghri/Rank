aa9a63310b590a89e649299801c3669ee9a5f686
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-12 20:34:32 +0100

    Addin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import { Home } from './Home/Home';
 import Auth from './Auth/Auth';
+import { Calculator } from './Calculator/Calculator';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Auth/>} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/calculator" element={<Calculator/>} />
       </Routes>
     </Router>
   );
