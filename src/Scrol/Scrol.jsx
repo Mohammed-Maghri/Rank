0dacd287dd728afbee309640a371a2c975653539
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-14 11:31:13 +0100

    Pushing This

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -26,6 +26,7 @@ import { WiDaySnow } from "react-icons/wi";
 import promo from "../clips/18.png"
 import kta from "../clips/kta.png"
 import meme from "../clips/memem.jpeg";
+import abdel from "../clips/abdel.png";
 
 export default function Scrol({objectvalue}) {
     const [visible, setVisible] = useState(false);
@@ -110,6 +111,9 @@ export default function Scrol({objectvalue}) {
             <div style={{borderRight : '0px'}}className=" ml-[5px] flex items-center border-solid border-yellow-500 border-[3px] justify-center   xs:w-[118px] xs:min-w-[118px] rounded-l-[25px] md:w-[118px] md:min-w-[118px] h-[115px]">
               
               {loadingstate == false ? (
+                item.login == "Ychahbi" ? (
+                  <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={abdel}/>
+                ):
                 item.login == "sel-jett" ? (
                   <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={kta}/>
                 ):item.login == "mmaghri" ? (
