4e14ecacb88b585ac84b7082f82f298035fdfcda
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-14 11:37:44 +0100

    Pushing This

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -27,6 +27,7 @@ import promo from "../clips/18.png"
 import kta from "../clips/kta.png"
 import meme from "../clips/memem.jpeg";
 import abdel from "../clips/abel.jpeg";
+import notab from "../clips/notab.png";
 
 export default function Scrol({objectvalue}) {
     const [visible, setVisible] = useState(false);
@@ -111,6 +112,9 @@ export default function Scrol({objectvalue}) {
             <div style={{borderRight : '0px'}}className=" ml-[5px] flex items-center border-solid border-yellow-500 border-[3px] justify-center   xs:w-[118px] xs:min-w-[118px] rounded-l-[25px] md:w-[118px] md:min-w-[118px] h-[115px]">
               
               {loadingstate == false ? (
+                item.login == "abel-all" ? (
+                  <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={notab}/>
+                ):
                 item.login == "ychahbi" ? (
                   <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={abdel}/>
                 ):
