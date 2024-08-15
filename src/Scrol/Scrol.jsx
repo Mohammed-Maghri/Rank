4bd6bfb9de3d8f86017c82740928232eaf4ac2a4
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-15 13:07:39 +0100

    Putting it tpo default

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -1,4 +1,39 @@
-import "../Scrol/Imports";
+import React, { useContext, useEffect } from "react";
+import { useState } from "react";
+import { IoSettings } from "react-icons/io5";
+import pic from "../clips/mmaghri.jpg";
+import { IoMdLogOut } from "react-icons/io";
+import {functionVisible, Nav} from "../Nav/Nav";
+import { FaChevronDown } from "react-icons/fa";
+import { Selector } from "../Selector/Slector";
+import taj from "../clips/crown.png";
+import redirect from "../clips/maximize.png";
+import { context } from "../context";
+import { PiInstagramLogoFill } from "react-icons/pi";
+import "./Scrol.css";
+import me from "../clips/this.png";
+import loua from "../clips/loua.png";
+import asad from "../clips/asad.png";
+import Cookies from "js-cookie";
+import mma from "../clips/mmaghri.jpg"
+import cat from "../clips/taj.jpeg"
+import first from "../clips/crown.png"
+import sec from "../clips/22.png"
+import third from "../clips/33.png"
+import king from "../clips/pass.png"
+import moz from "../clips/moz.png"
+import { WiDaySnow } from "react-icons/wi";
+import promo from "../clips/18.png"
+import kta from "../clips/kta.png"
+import meme from "../clips/memem.jpeg";
+import abdel from "../clips/abel.jpeg";
+import notab from "../clips/notab.png";
+import hamza from "../clips/pas.png";
+import yas from "../clips/yas.png";
+import pilo from "../clips/pilo.png";
+import mod from "../clips/mod.png";
+import mso from "../clips/maso.png";
+import bou from "../clips/bou.png";
 
 export default function Scrol({objectvalue}) {
     const [visible, setVisible] = useState(false);
