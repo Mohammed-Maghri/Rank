689e593f2792339fe49f470570e507b1af3dcccf
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-15 11:09:40 +0100

    Adding somthing

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -32,6 +32,7 @@ import hamza from "../clips/pas.png";
 import yas from "../clips/yas.png";
 import pilo from "../clips/pilo.png";
 import mod from "../clips/mod.png";
+import mso from "../clips/maso.png";
 
 export default function Scrol({objectvalue}) {
     const [visible, setVisible] = useState(false);
@@ -115,6 +116,9 @@ export default function Scrol({objectvalue}) {
             <div style={{borderRight : '0px'}}className=" ml-[5px] flex items-center border-solid border-yellow-500 border-[3px] justify-center   xs:w-[118px] xs:min-w-[118px] rounded-l-[25px] md:w-[118px] md:min-w-[118px] h-[115px]">
               
               {loadingstate == false ? (
+                item.login == "ymassiou" ? (
+                  <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={mso}/>
+                ):
                 item.login == "mkimdil" ? (
                   <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={mod}/>
                 ):
