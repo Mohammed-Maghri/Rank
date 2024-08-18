89b19af7b11d0faaede09951bdd1320c7a932380
Mohammed maghri <ouedzoum69@gmail.com>
2024-08-18 16:30:47 +0100

    Adding a Pic

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -34,7 +34,7 @@ import pilo from "../clips/pilo.png";
 import mod from "../clips/mod.png";
 import mso from "../clips/maso.png";
 import bou from "../clips/bou.png";
-
+import zech from "../clips/zech.png";
 export default function Scrol({objectvalue}) {
     const {logs, setLogs} = useContext(context);
     const {all, setAll} = useContext(context);
@@ -114,6 +114,9 @@ export default function Scrol({objectvalue}) {
             <div style={{borderRight : '0px'}}className=" ml-[5px] flex items-center border-solid border-yellow-500 border-[3px] justify-center   xs:w-[118px] xs:min-w-[118px] rounded-l-[25px] md:w-[118px] md:min-w-[118px] h-[115px]">
               
               {loadingstate == false ? (
+                item.login == "zech-chi" ? (
+                  <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={zech}/>
+                ):
                 item.login == "aboukdid" ? (
                   <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={bou}/>
                 ):
