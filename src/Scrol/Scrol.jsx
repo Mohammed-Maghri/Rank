9b9e4e8b8fb262cedd9f9c757f9eae678ef71a93
Mohammed maghri <ouedzoum69@gmail.com>
2024-09-20 16:17:18 +0100

    Adding some changes

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -43,6 +43,7 @@ import ala from "../clips/ala.png"
 import rd from "../clips/rd.png"
 import { GiFullMotorcycleHelmet } from "react-icons/gi";
 import ad from "../clips/ad.png"
+import abk from "../clips/abk.png"
 
 export default function Scrol({objectvalue}) {
     const {logs, setLogs} = useContext(context);
@@ -124,6 +125,9 @@ export default function Scrol({objectvalue}) {
             <div style={{borderRight : '0px'}}className=" ml-[5px] flex items-center border-solid border-yellow-500 border-[3px] justify-center   xs:w-[118px] xs:min-w-[118px] rounded-l-[25px] md:w-[118px] md:min-w-[118px] h-[115px]">
               
               {loadingstate == false ? (
+                item.login == "abkacimi" ? (
+                <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={abk}/>
+                ):
                 item.login == "amabrouk" ? (
                 <img  className="w-[97%] h-[108px] min-h-[105px] object-cover rounded-l-[20px]" src={ad}/>
                 ):
