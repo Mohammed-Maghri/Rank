28401a2e2ace85a843f2e8e876aa2c45daa3c7e3
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-09 14:59:22 +0100

    Backend Enppoint Changed

--- src/Dropdown/Dropdown.jsx
+++ src/Dropdown/Dropdown.jsx
@@ -90,7 +90,7 @@ const Componnent = ({on}) => {
             <div className="rounded-[8px] w-[100%] h-[50%] bg-slate-900">
             <div className="flex items-center pt-2 justify-center   h-[50%]">
               {(items.project_gitlab_path == null) ? <p  className="font-bars2 text-white z-20 font-bold "> EXAM </p> :
-              <p  className="font-bars2 text-white z-20 font-bold "> {items.project_gitlab_path.substring(items.project_gitlab_path.lastIndexOf('/') + 1, 20)} </p>
+              <p  className="font-bars2 text-white z-20 font-bold "> {items.project_gitlab_path.substring(items.project_gitlab_path.lastIndexOf('/') + 1)} </p>
               }
             </div>
             <div className=" h-[50%] rounded-[10px] flex items-center justify-center" >
