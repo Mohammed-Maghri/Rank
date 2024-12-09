2742b7dc79772119c67ebe7a399a1e946935641f
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-09 14:52:32 +0100

    Backend Enppoint Changed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -94,7 +94,9 @@ export function Home() {
 			|| data.login  == "mlouazir" 
 			|| data.login  == "amokhtar"
 			 || data.login == "moudrib" 
-			 || data.login == "alaassir" || data.login == "abablil"){
+			 || data.login == "alaassir" 
+			 || data.login == "abablil" 
+			 || data.login == "aghounam"){
 				// Read MZN hna 
 				Cookies.set("PoorLittleMan", "CRYINTHECORNER");
 				Cookies.set("125365486365864788498945187484584184451284518484168456848452487687846846868456879684688548", "648645684468463565341684565331546845168456531686843486");
