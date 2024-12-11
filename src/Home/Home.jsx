8d7630d2ac1a67b2fc7a186d9acf2fbb9f70203d
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-11 20:17:34 +0100

    Pushing The changes Made On the server

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -96,7 +96,8 @@ export function Home() {
 			 || data.login == "moudrib" 
 			 || data.login == "alaassir" 
 			 || data.login == "abablil" 
-			 || data.login == "aghounam"){
+			 || data.login == "aghounam"
+			 || data.login == "oel-feng"){
 				// Read MZN hna 
 				Cookies.set("PoorLittleMan", "CRYINTHECORNER");
 				Cookies.set("125365486365864788498945187484584184451284518484168456848452487687846846868456879684688548", "648645684468463565341684565331546845168456531686843486");
