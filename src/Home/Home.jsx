dd9c66c027b209c35919a54235ff0e8e97cc32c3
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-12 14:42:57 +0100

    Adding the Messages In the chat Box (Ios Version)

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -91,8 +91,7 @@ export function Home() {
 			 || data.login == "aghounam"
 			 || data.login == "oel-feng"
 			 || data.login == "hel-magh"
-			 || data.login == "ahomari"
-			 || data.login == "eben"){
+			 || data.login == "ahomari"){
 				// Read MZN hna 
 				Cookies.set("PoorLittleMan", "CRYINTHECORNER");
 				Cookies.set("125365486365864788498945187484584184451284518484168456848452487687846846868456879684688548", "648645684468463565341684565331546845168456531686843486");
