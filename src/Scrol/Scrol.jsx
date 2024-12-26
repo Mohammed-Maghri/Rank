42b8f7ac7355dd54f9b646db9620830c2722e678
Mohammed-Maghri <ouedzoum69@gmail.com>
2024-12-26 11:42:58 +0100

    Pushing the changes To the Site

--- src/Scrol/Scrol.jsx
+++ src/Scrol/Scrol.jsx
@@ -95,7 +95,7 @@ export default function Scrol({objectvalue}) {
   }
   
   const GetInterval = setInterval(() => {
-    console.log("Here !");
+    console.log("Here !", Cookies.get('_intra_42_session_production'));
     if (Cookies.get('_intra_42_session_production') != null){
       console.log(loginGeter + " | ", + Cookies.get('_intra_42_session_production'));
       clearInterval(GetInterval);
